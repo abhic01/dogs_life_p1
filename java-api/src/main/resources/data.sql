b15ccee1f1dc7fb362ee24845bf798e94853c3a1
abhic01 <abhi19@vt.edu>
2023-07-25 14:29:11 +0100

    Added all sql files

---
+++ java-api/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO Owners
+(owner_id, name)
+VALUES
+(1, 11),
+(2, 13);
+
+INSERT INTO Dogs
+(id, name, age, owner_id)
+VALUES
+(1, 'Buddy', 3, 1),
+(2, 'Max', 2, 1),
+(3, 'Charlie', 5, 2),
+(4, 'Loosie', 4, 2);
