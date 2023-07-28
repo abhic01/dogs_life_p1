333f3c354d615368123e02677364b308dbc172c6
Dylan <mail@db.com>
2023-07-28 16:43:32 +0100

    here

---
+++ Ex02-Objects/src/com/qa/TestBooks.java
@@ -0,0 +1,19 @@
+package com.qa;
+import java.util.Scanner;
+    public class TestBooks {
+
+        public static void main(String[] args) {
+            
+            int x = 3;
+            int z = 4;
+            double y = x + z;
+
+            if (x > y && x != z){
+                System.out.println("yes");
+            }
+            else {
+                System.out.println("no");
+            }
+    }
+
+        }
