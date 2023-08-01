0ebf2e8ab112fe058d0c9fc706a7bec43b03ee42
abhic01 <abhi20jun@gmail.com>
2023-08-01 14:51:55 +0100

    QL3

--- superheroes-ui/src/components/HeroDetail.jsx
+++ superheroes-ui/src/components/HeroDetail.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import batman from '../images/batman.jpeg';
+import images from '../index.js';
 
-const HeroDetail = () => {
+const HeroDetail = (props) => {
   return (
     <div>
-      <img src={batman} alt="Batman" height="250px"/>
-      <p id="blue-alias">Alias: Batman</p>
-      <p class="green-class">Name: Bruce Wayne</p>
-      <button class="green-class">Like</button>   
+      <img src={images[props.info.alias.replaceAll(/\s/g,'')]} alt={props.info.name} height="250px"/>      <p id="blue-alias">Alias: {props.info.alias}</p>
+      <p className="green-class">Name: {props.info.name}</p>
+      <button className="green-class">Like</button> 
     </div>
   )
 }
